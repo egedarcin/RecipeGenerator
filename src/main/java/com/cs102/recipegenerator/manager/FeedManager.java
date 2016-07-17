19099667c6f36cfea36f01ad38dfcbc8263ec0e2
Ege Darcin <edarcin@gmail.com>
2016-07-17 17:05:34 +0300

    Added Feed Manager

---
+++ src/main/java/com/cs102/recipegenerator/manager/FeedManager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cs102.recipegenerator.manager;
+import com.cs102.recipegenerator.domain.RecipeFeed;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+/**
+ *
+ * @author egedarcho
+ */
+//hejeasd
+public class FeedManager {
+    
+}
