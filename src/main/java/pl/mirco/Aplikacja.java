1788eb7f34b23bfa98e9022b8409eec232fe2d8c
Ege Darcin <edarcin@gmail.com>
2016-07-20 16:47:13 +0300

    ingredient fridgeitem naneleri

--- src/main/java/pl/mirco/Aplikacja.java
+++ src/main/java/pl/mirco/Aplikacja.java
@@ -3,6 +3,9 @@ package pl.mirco;
 import com.cs102.recipegenerator.domain.Fridge;
 import com.cs102.recipegenerator.domain.User;
 import com.cs102.recipegenerator.manager.UserManager;
+import com.cs102.recipegenerator.domain.FridgeItem;
+import com.cs102.recipegenerator.domain.Ingredient;
+        
 import java.awt.EventQueue;
 import java.util.List;
 
@@ -24,16 +27,19 @@ public class Aplikacja {
      * Launch the application.
      */
     public static void main(String[] args) {
-
+        List<FridgeItem> items = {potato, zucchini, tomatoes, garlic};
         Fridge fridge = new Fridge();
-
+        
         User user1 = new User();
         user1.setUsername("Ege");
         user1.setPassword("Darcin");
         user1.setFridge(fridge);
+        fridge.setItems(items);
+        
 
 //		 saving objects to session
         UserManager.saveUser(user1);
+        
 
         System.out.print(UserManager.getAllUsers());
     }
