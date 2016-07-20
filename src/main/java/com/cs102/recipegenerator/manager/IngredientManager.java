5f29301d9bfb5f329cf2404b2efd0391e3db00d5
Ege Darcin <edarcin@gmail.com>
2016-07-20 15:36:49 +0300

    ingredient management vol.1

--- src/main/java/com/cs102/recipegenerator/manager/IngredientManager.java
+++ src/main/java/com/cs102/recipegenerator/manager/IngredientManager.java
@@ -6,6 +6,7 @@
 package com.cs102.recipegenerator.manager;
 
 import com.cs102.recipegenerator.domain.FridgeItem;
+import com.cs102.recipegenerator.domain.Ingredient;
 import java.util.List;
 import org.hibernate.Session;
 
@@ -23,4 +24,10 @@ public class IngredientManager {
         return fridgeItems;
 
     }
+      public static void putIngredients(Ingredient ingredient){
+        Session ss = SessionManager.getSession();
+        ss.save(ingredient);
+        ss.getTransaction().commit();
+        ss.close();
+      }
 }
