162d4fdc09681ebb8ef9b7bcdbf4c05b35624b4c
Ege Darcin <edarcin@gmail.com>
2016-07-22 02:21:07 +0300

    Recipe saving improved

--- src/main/java/com/cs102/recipegenerator/domain/User.java
+++ src/main/java/com/cs102/recipegenerator/domain/User.java
@@ -1,6 +1,7 @@
 package com.cs102.recipegenerator.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
@@ -26,7 +27,9 @@ public class User implements Serializable {
     private String password;
 
     @OneToMany
-    private List<Recipe> LastRecipes;
+    private List<Recipe> LastRecipes = new ArrayList<Recipe>();
+    @OneToMany
+    private List<Recipe> MyFavourites = new ArrayList<Recipe>();
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Fridge fridge;
@@ -73,5 +76,11 @@ public class User implements Serializable {
 
     public void addLastRecipe(Recipe LastRecipe) {
         LastRecipes.add(LastRecipe);
+    }
+    public void setMyFavourites(List<Recipe> MyFavourites) {
+        this.MyFavourites = MyFavourites;
+    }
+    public void addFavouriteRecipe(Recipe FavRecipe){
+        MyFavourites.add(FavRecipe);
     }
 }
